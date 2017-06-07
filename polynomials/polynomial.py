e8c326a5a83d7fd62526c054031ae66380d8cc06
Student <student@WAK-STU-C02NM33DG083.local>
2017-06-07 12:18:56 -0400

    Finished polynomial.py (now I gotta just work with 0 and negative numbers)

--- polynomials/polynomial.py
+++ polynomials/polynomial.py
@@ -21,6 +21,7 @@ def get_opp(p, i):
     else:
         return ' - '
 
+
 def get_xterm(p, i):
     """
       >>> get_xterm((8, 6, 7, 9), 0)
@@ -70,6 +71,61 @@ def poly_to_str(p):
         polystr += '{0}{1}{2}'.format(opp, coeff, xterm)
 
     return polystr
+
+
+def add_polys(p1, p2):
+    """
+      >>> add_polys((3, 1, 2), (1, 2, 3))
+      (4, 3, 5)
+      >>> poly_to_str(add_polys((4, 3, 5, 9), (2, -3, 2, -9)))
+      '6x^3 + 7x'
+      >>> add_polys((5, 3, 1, 2), (1, 2, 3))
+      (5, 4, 3, 5)
+      >>> add_polys((5, 3), (1, 2, 3, 4))
+      (1, 2, 8, 7)
+    """
+    if len(p1) < len(p2):
+        p1 = ((0, ) * (len(p2) - len(p1))) + p1
+    elif len(p2) < len(p1):
+        p2 = ((0, ) * (len(p1) - len(p2))) + p2
+    p3 = []
+    for i in range(len(p1)):
+        p3.append(p1[i] + p2[i])
+    
+    return tuple(p3)
+
+
+def term_x_poly(coeff, exp, p):
+    """
+      >>> term_x_poly(4, 2, (3, 0, 2, 0, 0, 0))
+      (12, 0, 8, 0, 0, 0, 0, 0)
+    """
+    prod = []
+
+    for c in p:
+        prod.append(coeff * c)
+
+    return tuple(prod) + (0, ) * exp
+
+
+def mul_polys(p1, p2):
+    """
+      >>> mul_polys((4, -5), (2, 3, -6))
+      (8, 2, -39, 30)
+      >>> mul_polys((3, 2), (5, 6))
+      (15, 28, 12)
+      >>> mul_polys((4, 0, 5, 3), (7, 4, 0, 5))
+      (28, 16, 35, 61, 12, 25, 15)
+      >>> mul_polys((0, 4), (4, 0))
+      (16, 0)
+      >>> mul_polys((0, 0, 0, 0, 0, 4), (4, 0, 0, 0, 0, 0))
+      (16, 0, 0, 0, 0, 0)
+    """
+    p3 = ()
+    for i in range(len(p1)):
+        bigpoly = term_x_poly(p1[i], len(p1) - 1 - i, p2)
+        p3 = add_polys(bigpoly, p3)
+    return (p3)
 
 
 if __name__ == '__main__':
